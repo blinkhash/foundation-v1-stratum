e55b303eee69a45a6cbf2a86429a6c256c0cebc7
Blinkhash <blinkhashpool@gmail.com>
2021-11-17 21:56:13 -0500

    Modify ghostrider multiplier, increment version

--- scripts/main/algorithms.js
+++ scripts/main/algorithms.js
@@ -216,6 +216,7 @@ const Algorithms = {
 
   // Ghostrider Algorithm
   'ghostrider': {
+    multiplier: Math.pow(2, 16),
     diff: parseInt('0x00000000ffff0000000000000000000000000000000000000000000000000000'),
     hash: function() {
       return function() {
--- scripts/test/algorithms.test.js
+++ scripts/test/algorithms.test.js
@@ -196,7 +196,7 @@ describe('Test algorithm functionality', () => {
   test('Test implemented ghostrider algorithm', () => {
     const start = Buffer.from('000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f');
     const main = Buffer.from('affb18b37144a5829c610c249b8f2b165aabcc27eeb39c4f3b4d07c0bb431bcc', 'hex');
-    const output = Buffer.from('e03d2f02c18d61df4af9b180f48f7c9e3ee2b00a73a0b5dff72afb65fcf9d49a', 'hex');
+    const output = Buffer.from('5438a6e89f606117ca8024cda972a0fc134a718fba8908ce961e8963d75a4bdc', 'hex');
     expect(Algorithms.ghostrider.hash({}).apply(null, [start, main]).length).toBe(32);
     expect(Algorithms.ghostrider.hash({}).apply(null, [start, main])).toStrictEqual(output);
   });
